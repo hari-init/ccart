551b3eaf8803dea03b5c6079aa947c5cbf5453db
Hari <140445632+hari-init@users.noreply.github.com>
2023-12-13 20:40:51 -0500

    updated the curd

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -3,12 +3,85 @@ $(document).ready(() => {
     const data = {
         "stores": [
             {
-                "storeId": "ST987001",
-                "name": "Elegant Emporium",
+                "storeId": "ST01",
+                "name": "Hari's store",
                 "address": "789 Chic Avenue, Glam City, GC",
-                "contactEmail": "elegant.emporium@storemail.com",
-                "type": "Luxury Boutique",
-                "ownerName": "Victoria Elegance"
+                "contactEmail": "hari@test.com",
+                "type": "Ice Cream Store",
+                "ownerName": "Victoria Elegance",
+                "stocks": [
+                    {
+                        "id": 1,
+                        "flavor": "Vanilla",
+                        "description": "Classic vanilla flavor with a rich and creamy taste. Perfect for those who love the timeless simplicity of vanilla.",
+                        "quantity": 100,
+                        "price": 2.50,
+                        "allergenInfo": ["milk"],
+                        "featured": true
+                    },
+                    {
+                        "id": 2,
+                        "flavor": "Chocolate",
+                        "description": "Indulge in the richness of our chocolate ice cream. Made with premium cocoa for a delightful chocolate experience.",
+                        "quantity": 80,
+                        "price": 3.00,
+                        "allergenInfo": ["milk", "soy"],
+                        "featured": false
+                    },
+                    {
+                        "id": 3,
+                        "flavor": "Strawberry",
+                        "description": "Experience the freshness of ripe strawberries in every scoop. A fruity and delightful treat for strawberry lovers.",
+                        "quantity": 120,
+                        "price": 3.50,
+                        "allergenInfo": ["milk"],
+                        "featured": true
+                    },
+                    {
+                        "id": 4,
+                        "flavor": "Mint Chocolate Chip",
+                        "description": "Cool and refreshing mint ice cream combined with decadent chocolate chips. A perfect blend of flavors.",
+                        "quantity": 60,
+                        "price": 4.00,
+                        "allergenInfo": ["milk"],
+                        "featured": false
+                    },
+                    {
+                        "id": 5,
+                        "flavor": "Cookies and Cream",
+                        "description": "Irresistible combination of vanilla ice cream with chunks of chocolate cookies. A classic favorite for cookie enthusiasts.",
+                        "quantity": 90,
+                        "price": 4.50,
+                        "allergenInfo": ["milk", "wheat"],
+                        "featured": true
+                    },
+                    {
+                        "id": 6,
+                        "flavor": "Rocky Road",
+                        "description": "A delightful mix of chocolate ice cream, marshmallows, and nuts. Indulge in the rocky road experience!",
+                        "quantity": 70,
+                        "price": 4.25,
+                        "allergenInfo": ["milk", "nuts"],
+                        "featured": false
+                    },
+                    {
+                        "id": 7,
+                        "flavor": "Butter Pecan",
+                        "description": "Rich buttery ice cream with crunchy pecans. A perfect blend of sweet and nutty flavors.",
+                        "quantity": 50,
+                        "price": 4.75,
+                        "allergenInfo": ["milk", "nuts"],
+                        "featured": true
+                    },
+                    {
+                        "id": 8,
+                        "flavor": "Pistachio",
+                        "description": "Smooth pistachio-flavored ice cream with real pistachio pieces. A delightful treat for pistachio lovers.",
+                        "quantity": 65,
+                        "price": 5.00,
+                        "allergenInfo": ["milk", "nuts"],
+                        "featured": false
+                    }]
             },
             {
                 "storeId": "ST987002",
@@ -109,13 +182,18 @@ $(document).ready(() => {
             }
         ]
     }
+    let adminData;
 
-    localStorage.setItem('adminData', JSON.stringify(data));
-    const adminData = JSON.parse(localStorage.getItem('adminData'));
+    if (localStorage.getItem('adminData')) {
+        adminData = JSON.parse(localStorage.getItem('adminData'));
+    } else {
+        localStorage.setItem('adminData', JSON.stringify(data));
+    }
+
     $("#storeGrid").jsGrid({
         height: "600px",
         width: "100%",
-
+        name: 'stores',
         inserting: true,
         filtering: true,
         editing: true,
@@ -130,15 +208,46 @@ $(document).ready(() => {
             { name: "address", align: "center", title: "Address", type: "text", width: 90, sorting: false },
             { name: "type", align: "center", title: "Store Type", type: "text", width: 70, sorting: false },
             { name: "ownerName", align: "center", title: "Owner", type: "text", width: 60, sorting: false },
-            { name: "contactEmail", align: "center", title: "Email", type: "email", width: 100, title: "Store Email", sorting: false },
+            { name: "contactEmail", align: "center", type: "text", width: 100, validate: "required", title: "Store Email", sorting: false },
             { type: "control" }
-        ]
+        ],
+        onItemInserted: (node) => {
+            if (node.grid.name === 'stores') {
+                adminData.stores.unshift(node.item);
+                localStorage.setItem('adminData', JSON.stringify(adminData));
+                $("#storeGrid").jsGrid("refresh");
+            }
+        },
+        onItemDeleted: (node) => {
+            if (node.grid.name === 'stores') {
+                for (let i = 0; i < adminData.stores.length; i++) {
+                    if (adminData.stores[i].storeId === node.item.storeId) {
+                        adminData.stores.splice(i, 1);
+                        break;
+                    }
+                    localStorage.setItem('adminData', JSON.stringify(adminData));
+                    $("#storeGrid").jsGrid("refresh");
+                }
+            }
+        },
+        onItemUpdated: (node) => {
+            if (node.grid.name === 'stores') {
+                for (let i = 0; i < adminData.stores.length; i++) {
+                    if (adminData.stores[i].storeId === node.item.storeId) {
+                        adminData.stores[i] = node.item;
+                        break;
+                    }
+                }
+                localStorage.setItem('adminData', JSON.stringify(adminData));
+                $("#storeGrid").jsGrid("refresh");
+            }
+        }
     });
 
     $("#ownerGrid").jsGrid({
         height: "600px",
         width: "100%",
-
+        name: 'owners',
         inserting: true,
         filtering: true,
         editing: true,
@@ -154,7 +263,39 @@ $(document).ready(() => {
             { name: "password", align: "center", title: "Password", type: "text", width: 90, sorting: false },
             { name: "contactEmail", align: "center", title: "Email", type: "email", width: 100, title: "Store Email", sorting: false },
             { type: "control" }
-        ]
+        ],
+
+        onItemInserted: (node) => {
+            if (node.grid.name === 'owners') {
+                adminData.owners.unshift(node.item);
+                localStorage.setItem('adminData', JSON.stringify(adminData));
+                $("#ownerGrid").jsGrid("refresh");
+            }
+        },
+        onItemDeleted: (node) => {
+            if (node.grid.name === 'owners') {
+                for (let i = 0; i < adminData.owners.length; i++) {
+                    if (adminData.owners[i].storeId === node.item.storeId) {
+                        adminData.owners.splice(i, 1);
+                        break;
+                    }
+                    localStorage.setItem('adminData', JSON.stringify(adminData));
+                    $("#ownerGrid").jsGrid("refresh");
+                }
+            }
+        },
+        onItemUpdated: (node) => {
+            if (node.grid.name === 'owners') {
+                for (let i = 0; i < adminData.owners.length; i++) {
+                    if (adminData.owners[i].storeId === node.item.storeId) {
+                        adminData.owners[i] = node.item;
+                        break;
+                    }
+                }
+                localStorage.setItem('adminData', JSON.stringify(adminData));
+                $("#ownerGrid").jsGrid("refresh");
+            }
+        }
     });
 });
 
--- pages/owner/owner.js
+++ pages/owner/owner.js
@@ -1,84 +1,17 @@
 $(document).ready(() => {
 
-    const stocks = [
-        {
-            "id": 1,
-            "flavor": "Vanilla",
-            "description": "Classic vanilla flavor with a rich and creamy taste. Perfect for those who love the timeless simplicity of vanilla.",
-            "quantity": 100,
-            "price": 2.50,
-            "allergenInfo": ["milk"],
-            "featured": true
-          },
-          {
-            "id": 2,
-            "flavor": "Chocolate",
-            "description": "Indulge in the richness of our chocolate ice cream. Made with premium cocoa for a delightful chocolate experience.",
-            "quantity": 80,
-            "price": 3.00,
-            "allergenInfo": ["milk", "soy"],
-            "featured": false
-          },
-          {
-            "id": 3,
-            "flavor": "Strawberry",
-            "description": "Experience the freshness of ripe strawberries in every scoop. A fruity and delightful treat for strawberry lovers.",
-            "quantity": 120,
-            "price": 3.50,
-            "allergenInfo": ["milk"],
-            "featured": true
-          },
-          {
-            "id": 4,
-            "flavor": "Mint Chocolate Chip",
-            "description": "Cool and refreshing mint ice cream combined with decadent chocolate chips. A perfect blend of flavors.",
-            "quantity": 60,
-            "price": 4.00,
-            "allergenInfo": ["milk"],
-            "featured": false
-          },
-          {
-            "id": 5,
-            "flavor": "Cookies and Cream",
-            "description": "Irresistible combination of vanilla ice cream with chunks of chocolate cookies. A classic favorite for cookie enthusiasts.",
-            "quantity": 90,
-            "price": 4.50,
-            "allergenInfo": ["milk", "wheat"],
-            "featured": true
-          },
-          {
-            "id": 6,
-            "flavor": "Rocky Road",
-            "description": "A delightful mix of chocolate ice cream, marshmallows, and nuts. Indulge in the rocky road experience!",
-            "quantity": 70,
-            "price": 4.25,
-            "allergenInfo": ["milk", "nuts"],
-            "featured": false
-          },
-          {
-            "id": 7,
-            "flavor": "Butter Pecan",
-            "description": "Rich buttery ice cream with crunchy pecans. A perfect blend of sweet and nutty flavors.",
-            "quantity": 50,
-            "price": 4.75,
-            "allergenInfo": ["milk", "nuts"],
-            "featured": true
-          },
-          {
-            "id": 8,
-            "flavor": "Pistachio",
-            "description": "Smooth pistachio-flavored ice cream with real pistachio pieces. A delightful treat for pistachio lovers.",
-            "quantity": 65,
-            "price": 5.00,
-            "allergenInfo": ["milk", "nuts"],
-            "featured": false
-          }
-    ]
+  let adminData;
+  let stocks;
+
+  if (localStorage.getItem('adminData')) {
+      adminData = JSON.parse(localStorage.getItem('adminData'));
+      stocks = adminData.stores[0].stocks
+  } 
 
     $("#stockGrid").jsGrid({
         height: "600px",
         width: "100%",
-
+        name:  'stocks',
         inserting: true,
         filtering: true,
         editing: true,
@@ -94,7 +27,41 @@ $(document).ready(() => {
             { name: "price", align: "center", title: "Price", type: "text", width: 90, sorting: false },
             { name: "allergenInfo", align: "center", title: "Allergen Info", type: "array", width: 90, sorting: false },
             { type: "control" }
-        ]
+        ],
+        onItemInserted: (node) => {
+          if (node.grid.name === 'stocks') {
+              stocks.unshift(node.item);
+              adminData.stores[0].stocks = stocks; // need to update with dynamic data
+              localStorage.setItem('adminData', JSON.stringify(adminData));
+              $("#storeGrid").jsGrid("refresh");
+          }
+      },
+      onItemDeleted: (node) => {
+          if (node.grid.name === 'stocks') {
+              for (let i = 0; i < stocks.length; i++) {
+                  if (stocks[i].id === node.item.id) {
+                     stocks.splice(i, 1);
+                      break;
+                  }
+                  adminData.stores[0].stocks = stocks; // need to update with dynamic data
+                  localStorage.setItem('adminData', JSON.stringify(adminData));
+                  $("#storeGrid").jsGrid("refresh");
+              }
+          }
+      },
+      onItemUpdated: (node) => {
+          if (node.grid.name === 'stocks') {
+              for (let i = 0; i < stocks.length; i++) {
+                  if (stocks[i].id === node.item.id) {
+                      stocks[i] = node.item;
+                      break;
+                  }
+              }
+              adminData.stores[0].stocks = stocks; // need to update with dynamic data
+              localStorage.setItem('adminData', JSON.stringify(adminData));
+              $("#storeGrid").jsGrid("refresh");
+          }
+      }
     });
 
-})
+});
